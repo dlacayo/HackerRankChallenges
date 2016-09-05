b1d942aa52d475fe7ae02585547f8d9a6b715cf7
Daniel Lacayo <daniel@lacayo.cr>
2016-09-05 08:38:29 -0600

    Added the solution for A Very Big Sum Challenge

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+        if (n==arr.Length)
+            Console.WriteLine(AddNumbers(arr));
+    }
+
+    private static long AddNumbers(int[] arr)
+    {
+        long sum = 0;
+        foreach (int i in arr)
+            sum += i;
+
+        return sum;
+    }
+}
