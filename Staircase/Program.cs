775a92f11ac34df688ad9b2eb892922ee0309309
Daniel Lacayo <daniel@lacayo.cr>
2016-09-07 09:44:20 -0600

    Added New Challenges: - Staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        PrintStaircase(size);
+    }
+
+    private static void PrintStaircase(int size)
+    {
+        for(int i=0;i<size;i++)
+            PrintLevel(size-(1+i), 1+i);
+    }
+
+    private static void PrintLevel(int numberOfSpaces, int numberOfSymbols)
+    {
+        for(int i=0;i<numberOfSpaces;i++)
+            Console.Write(" ");
+        for (int i = 0; i < numberOfSymbols; i++)
+            Console.Write("#");
+        Console.Write("\n");
+
+    }
+}
