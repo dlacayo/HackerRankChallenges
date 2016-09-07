30fe7e1f98f09a748db54ac9f5c31007d5755c25
Daniel Lacayo <daniel@lacayo.cr>
2016-09-06 18:03:05 -0600

    Added New Challenges: - Diagonal Difference - PlusMinus

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[][] squareMatrix = new int[size][];
+        for (int row = 0; row < size; row++)
+        {
+            string[] columnValues = Console.ReadLine().Split(' ');
+            squareMatrix[row] = Array.ConvertAll(columnValues, Int32.Parse);
+        }
+
+        int diagonalDifference = CalculateDiagonalDifference(squareMatrix);
+        Console.WriteLine(diagonalDifference);
+    }
+
+    private static int CalculateDiagonalDifference(int[][] squareMatrix)
+    {
+        int size = squareMatrix[0].Length;
+
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            primaryDiagonalSum += squareMatrix[i][i];
+            int j = size - (1 + i);
+            secondaryDiagonalSum += squareMatrix[i][j];
+        }
+
+        return Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+    }
+}
---
+++ PlusMinus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        if(size <1)
+            return;
+
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] values = Array.ConvertAll(arr_temp, Int32.Parse);
+
+        double positiveCount = 0;
+        double negativeCount = 0;
+        double zeroCount = 0;
+
+        ProcessArray(values, out positiveCount, out negativeCount, out zeroCount);
+
+        Console.WriteLine($"{positiveCount:F6}\n{negativeCount:F6}\n{zeroCount:F6}");
+    }
+
+    private static void ProcessArray(int[] values, out double positiveCount, out double negativeCount, out double zeroCount)
+    {
+        int size = values.Length;
+
+        zeroCount = positiveCount = negativeCount = 0;
+
+        foreach (int value in values)
+        {
+            if (value == 0)
+                zeroCount++;
+            else if (value > 0)
+                positiveCount++;
+            else
+                negativeCount++;
+        }
+
+        zeroCount /= size;
+        positiveCount /= size;
+        negativeCount /= size;
+    }
+}
