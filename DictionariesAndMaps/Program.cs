1d9742daf5da12cba96fafdb199120a82383f864
Daniel Lacayo <daniel@lacayo.cr>
2016-09-24 17:49:00 -0600

    Added Challenges - DictionariesAndMaps - Recursion

---
+++ DictionariesAndMaps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int numberOfEntries = Convert.ToInt32(Console.ReadLine());
+
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+        for (int i = 0; i < numberOfEntries; i++)
+        {
+            string[] d = Console.ReadLine().Split(' ');
+            phoneBook.Add(d[0],d[1]);
+        }
+
+        string key = Console.ReadLine();
+        while (!string.IsNullOrEmpty(key))
+        {
+            string value = "";
+
+            if (phoneBook.TryGetValue(key, out value))
+                Console.WriteLine($"{key}={value}");
+            else
+                Console.WriteLine("Not found");
+
+            key = Console.ReadLine();
+        }
+
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Factorial(number));
+    }
+
+    private static long Factorial(int number)
+    {
+        if (number <= 0)
+            return 1;
+        return number*Factorial(number - 1);
+    }
+}
