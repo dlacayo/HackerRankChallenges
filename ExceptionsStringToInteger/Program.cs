f3522ebbe3e4e6206b3baabf25a970c9dc6f2fff
Daniel Lacayo <daniel@lacayo.cr>
2016-10-06 14:14:21 -0600

    Added Challenges: Exceptions String To Integer More Exceptions

---
+++ ExceptionsStringToInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+        int number = 0;
+        try
+        {
+            number = Convert.ToInt32(S);
+            Console.WriteLine(number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
