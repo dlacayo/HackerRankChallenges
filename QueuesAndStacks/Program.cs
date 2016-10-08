72e3526a521194511a5690dcf0bcd88e8c87c58a
Daniel Lacayo <daniel@lacayo.cr>
2016-10-08 15:48:20 -0600

    Added Code to Queues and Stacks Challenge

--- QueuesAndStacks/Program.cs
+++ QueuesAndStacks/Program.cs
@@ -1,15 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace QueuesAndStacks
+class Solution
 {
-    class Program
+    private Stack<char> stack;
+    private Queue<char> queue;
+
+    public Solution()
     {
-        static void Main(string[] args)
+        stack=new Stack<char>();
+        queue = new Queue<char>();
+    }
+
+    private char dequeueCharacter()
+    {
+        return queue.Dequeue();
+    }
+
+    private char popCharacter()
+    {
+        return stack.Pop();
+    }
+
+    private void enqueueCharacter(char c)
+    {
+        queue.Enqueue(c);
+    }
+
+    private void pushCharacter(char c)
+    {
+        stack.Push(c);
+    }
+
+    static void Main(String[] args)
+    {
+        // read the string s.
+        string s = Console.ReadLine();
+
+        // create the Solution class object p.
+        Solution obj = new Solution();
+
+        // push/enqueue all the characters of string s to stack.
+        foreach (char c in s)
         {
+            obj.pushCharacter(c);
+            obj.enqueueCharacter(c);
+        }
+
+        bool isPalindrome = true;
+
+        // pop the top character from stack.
+        // dequeue the first character from queue.
+        // compare both the characters.
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (obj.popCharacter() != obj.dequeueCharacter())
+            {
+                isPalindrome = false;
+
+                break;
+            }
+        }
+
+        // finally print whether string s is palindrome or not.
+        if (isPalindrome)
+        {
+            Console.Write("The word, {0}, is a palindrome.", s);
+        }
+        else
+        {
+            Console.Write("The word, {0}, is not a palindrome.", s);
         }
     }
+
+   
+
+
+
+
+
 }
