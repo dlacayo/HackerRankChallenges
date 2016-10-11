46026ce2d50a77ab7c230d28e16357230c5d2cb4
Daniel Lacayo <daniel@lacayo.cr>
2016-10-11 13:00:50 -0600

    Added Challenges: - Happy Ladybugs - Maximum Perimeter Triangle

---
+++ HappyLadybugs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32.SafeHandles;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int Q = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < Q; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string b = Console.ReadLine();
+
+            CheckForUnhappyLadybugs(n, b);
+        }
+    }
+
+    private static void CheckForUnhappyLadybugs(int n, string s)
+    {
+        char[] separateLetters = s.ToCharArray();
+
+        if (s.Contains("_"))
+            Array.Sort(separateLetters);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (char.IsLetter(separateLetters[i]))
+            {
+                bool sameBefore = false;
+                bool sameAfter = false;
+                if (i > 0)
+                    sameBefore = separateLetters[i] == separateLetters[i - 1];
+                if (i < n - 1)
+                    sameAfter = separateLetters[i] == separateLetters[i + 1];
+
+                if (!sameBefore && !sameAfter)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("YES");
+    }
+}
---
+++ MaximumPerimeterTriangle/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int numberOfSticks = Convert.ToInt32(Console.ReadLine());
+        string[] s = Console.ReadLine().Split(' ');
+
+        //Stream inputStream = Console.OpenStandardInput();
+        //byte[] bytes = new byte[1536];    // 1.5kb
+        //int outputLength = inputStream.Read(bytes, 0, 1536);
+        //string[] s = System.Text.Encoding.UTF8.GetString(bytes).Split(' ');
+
+        long[] sticks = Array.ConvertAll(s, Int64.Parse);
+
+        MaximumPerimeterCalculator calculator = new MaximumPerimeterCalculator(numberOfSticks, sticks);
+        Console.WriteLine(calculator.CalculateMaximumPerimeterTriangle());
+    }
+
+    class MaximumPerimeterCalculator
+    {
+        private int _numberOfSticks;
+        private long[] _sticks;
+        private string _answer;
+
+        public MaximumPerimeterCalculator(int numberOfSticks, long[] sticks)
+        {
+            _numberOfSticks = numberOfSticks;
+            _sticks = sticks;
+            Array.Sort(sticks);
+            _answer = "-1";
+        }
+
+        public string CalculateMaximumPerimeterTriangle()
+        {
+            for (int i = 0; i < _numberOfSticks - 2; i++)
+                TestTriangle(_sticks[i], _sticks[i + 1], _sticks[i + 2]);
+
+            return _answer;
+        }
+
+        private void TestTriangle(long i, long j, long k)
+        {
+
+            if (SticksMakeValidTriangle(i, j, k))
+            {
+                long perimeter = i + j + k;
+                SetAnswer(i, j, k);
+            }
+        }
+
+        private void SetAnswer(long s1, long s2, long s3)
+        {
+            _answer = $"{s1} {s2} {s3}";
+        }
+
+        private bool SticksMakeValidTriangle(long i, long j, long k)
+        {
+            return (i + j > k && i + k > j && j + k > i);
+        }
+
+    }
+
+
+}
