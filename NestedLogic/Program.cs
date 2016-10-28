6c0b1978caaa96e9c72051acb8bfb11b23c52159
Daniel Lacayo <daniel@lacayo.cr>
2016-10-27 19:21:45 -0600

    Added Challenge: - Nested Logic

---
+++ NestedLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int[] returnDateParts = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+        int[] dueDateParts = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+
+        DateTime returnDate = new DateTime(returnDateParts[2], returnDateParts[1], returnDateParts[0]);
+        DateTime dueDate = new DateTime(dueDateParts[2], dueDateParts[1], dueDateParts[0]);
+
+        Console.WriteLine(CalculateFine(returnDate, dueDate));
+    }
+
+    private static int CalculateFine(DateTime returnDate, DateTime dueDate)
+    {
+        TimeSpan diff = dueDate - returnDate;
+
+        if (diff.TotalSeconds >= 0) // returned before or on due date
+            return 0;
+
+        if (returnDate.Year > dueDate.Year)
+            return 10000;
+
+        if (dueDate.Month != returnDate.Month)
+            return 500 * (returnDate.Month - dueDate.Month);
+
+        return Math.Abs(15 * diff.Days);
+    }
+}
