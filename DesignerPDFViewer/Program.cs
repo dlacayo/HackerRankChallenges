b67f712fc1bbba0b42e7cd92a4dabd500d33b199
Daniel Lacayo <daniel@lacayo.cr>
2016-11-23 13:04:45 -0600

    Added Challenge: - Designer PDF Viewer

---
+++ DesignerPDFViewer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] h_temp = Console.ReadLine().Split(' ');
+        int[] h = Array.ConvertAll(h_temp, Int32.Parse);
+        string word = Console.ReadLine();
+
+        int maxLetterHeight = 0;
+
+        foreach (char c in word)
+        {
+            int currentLetterHeight = h[(int) c - 97];
+            maxLetterHeight = currentLetterHeight > maxLetterHeight ? currentLetterHeight : maxLetterHeight;
+        }
+
+        Console.WriteLine(maxLetterHeight*word.Length);
+    }
+}
