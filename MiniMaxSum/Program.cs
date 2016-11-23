a56f3ce64946e0fb1a209aa2832f5d8a814f8d39
Daniel Lacayo <daniel@lacayo.cr>
2016-11-23 09:06:54 -0600

    Added challenge: - Mini-Max Sum

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string[] numbersAsStrings = Console.ReadLine().Split(' ');
+        long[] numbers = Array.ConvertAll(numbersAsStrings, Int64.Parse);
+        Array.Sort(numbers);
+
+        long minSum = 0;
+        for (int i = 0; i < numbers.Length - 1; i++)
+            minSum += numbers[i];
+
+        long maxSum = 0;
+        for (int i = 1; i < numbers.Length; i++)
+            maxSum += numbers[i];
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+}
