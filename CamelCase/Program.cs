53c363a697a32f11206149d1dc421c59c885f7a2
Daniel Lacayo <daniel@lacayo.cr>
2016-11-23 18:54:48 -0600

    Added Challenge: - CamelCase

---
+++ CamelCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+
+        int wordCount = 1;
+
+        foreach (char c in s)
+            if (char.IsUpper(c))
+                wordCount++;
+
+        Console.WriteLine(wordCount);
+    }
+}
