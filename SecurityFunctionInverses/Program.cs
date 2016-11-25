3682e71f6adabd9edfe2a2bfccec2ec4f3f96277
Daniel Lacayo <daniel@lacayo.cr>
2016-11-25 08:56:20 -0600

    Added Challenge: - Security Function Inverses

---
+++ SecurityFunctionInverses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+
+        for (int i = 1; i <= numbers.Length; i++)
+            for (int j = 0; j < numbers.Length; j++)
+                if (numbers[j] == i)
+                    Console.WriteLine(j+1);
+
+    }
+}
