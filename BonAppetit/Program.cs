1f88679de9d5d6c4d86cbb9bdeadf8b76d54af26
Daniel Lacayo <daniel@lacayo.cr>
2016-11-27 16:37:41 -0600

    Added Challenges: - Bon Appetit - Sock Merchant

---
+++ BonAppetit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string[] itemInfo = Console.ReadLine().Split(' ');
+        int numberOfItems = Convert.ToInt32(itemInfo[0]);
+        int itemNotEaten = Convert.ToInt32(itemInfo[1]);
+        string[] itemValues = Console.ReadLine().Split(' ');
+        int[] values = Array.ConvertAll(itemValues, Int32.Parse);
+        int amountCharged = Convert.ToInt32(Console.ReadLine());
+
+        int amountToSplitWithAnna = 0;
+        for (int i = 0; i < numberOfItems; i++)
+            if (i != itemNotEaten)
+                amountToSplitWithAnna += values[i];
+
+        int whatAnnaHasToPay = amountToSplitWithAnna/2;
+
+        Console.WriteLine(whatAnnaHasToPay == amountCharged ? "Bon Appetit":(amountCharged-whatAnnaHasToPay).ToString());
+
+    }
+}
---
+++ SockMerchant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] c_temp = Console.ReadLine().Split(' ');
+        int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+
+        Array.Sort(c);
+
+        int numberOfPairs = 0;
+        for (int i = 0; i < n-1; i++)
+        {
+            if (c[i] == c[i + 1])
+            {
+                numberOfPairs++;
+                i++;
+            }
+        }
+
+        Console.WriteLine(numberOfPairs);
+    }
+}
