71c7e82efe4ad34b6f239885e01ba8a1be2849c6
Daniel Lacayo <daniel@lacayo.cr>
2016-12-06 15:04:21 -0600

    Added Challenge: -Divisible Sum Pairs

---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(a_temp, Int32.Parse);
+        int numberOfPairs = 0;
+        Array.Sort(numbers);
+        for(int i=0;i<n-1;i++)
+            for (int j=i+1;j<n;j++)
+                if ((numbers[i] + numbers[j])%k == 0)
+                    numberOfPairs++;
+        Console.WriteLine(numberOfPairs);
+
+    }
+}
