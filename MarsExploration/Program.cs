5851edd2a49267f0f7f7a41f4f06919c15cc4bc0
Daniel Lacayo <daniel@lacayo.cr>
2016-12-07 17:23:03 -0600

    Added Challenges: - Mars Exploration - Repeated string

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int letterCount = 0;
+        string S = Console.ReadLine();
+        for (int i = 0; i < S.Length; i += 3)
+        {
+            if (S[i] != 'S') letterCount++;
+            if (S[i+1] != 'O') letterCount++;
+            if (S[i+2] != 'S') letterCount++;
+        }
+
+        Console.WriteLine(letterCount);
+    }
+}
---
+++ RepeatedString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long sLength = s.Length;
+
+        long numberOfAs = GetNumberOfA(s);
+
+        numberOfAs *= n/sLength;
+        long remainder = n%sLength;
+        numberOfAs += GetNumberOfA(s.Substring(0, (int)remainder));
+
+        Console.WriteLine(numberOfAs);
+
+    }
+
+    private static int GetNumberOfA(string s)
+    {
+        int n = 0;
+        foreach (var c in s)
+            if (c == 'a')
+                n++;
+        return n;
+    }
+}
