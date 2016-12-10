384b8d6087d40a6c80b934dc984b9d95ecd41a50
Daniel Lacayo <daniel@lacayo.cr>
2016-12-10 11:38:33 -0600

    Added Challenge: - Minimum Distances

---
+++ MinimumDistances/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] A_temp = Console.ReadLine().Split(' ');
+        int[] A = Array.ConvertAll(A_temp, Int32.Parse);
+
+        int minimumDistance = -1;
+
+        for(int i=0;i<n-1;i++)
+            for (int j = i + 1; j < n; j++)
+                if (A[i] == A[j])
+                {
+                    int distance = j - i;
+                    if (minimumDistance == -1)
+                        minimumDistance = distance;
+                    if (j - i < minimumDistance)
+                        minimumDistance = distance;
+                }
+        Console.WriteLine(minimumDistance);
+
+    }
+}
