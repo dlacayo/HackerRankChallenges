73a1c4159e185565d4402e6f4f945a503392a884
Daniel Lacayo <daniel@lacayo.cr>
2017-01-03 11:23:15 -0600

    Added Challenge: - Viral Advertising

---
+++ ViralAdvertising/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int days = Convert.ToInt32(Console.ReadLine());
+
+            int peopleSum = 0;
+            int peopleForNextDay = 5;
+            for (int i = 1; i <= days; i++)
+            {
+                int likesToday = peopleForNextDay / 2;
+                peopleSum += likesToday;
+                peopleForNextDay = likesToday * 3;
+            }
+
+            Console.WriteLine(peopleSum);
+        }
+    }
